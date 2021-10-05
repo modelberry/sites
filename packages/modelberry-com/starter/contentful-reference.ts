86554a3ab7cb1a75a721fbf58f0daf961d3f6033
Jacco Meijer <jacco@jaccomeijer.nl>
2021-10-05 19:26:09 +0200

    chore: remove starter

--- packages/modelberry-com/starter/contentful-reference.ts
+++
@@ -1,5 +0,0 @@
-export type ContentfulReference = {
-  sys: {
-    id: string
-  }
-}
--- packages/modelberry-com/starter/main-content.ts
+++
@@ -1,8 +0,0 @@
-import { globals } from './contentful-globals-content'
-import { page } from './contentful-page-content'
-import { navigationSection } from './contentful-navigation-section-content'
-import { navigationSegment } from './contentful-navigation-segment-content'
-import { textSection } from './contentful-text-section-content'
-import { topicSection } from './contentful-topic-section-content'
-import { topic } from './contentful-topic-content'
-import { navigationAction } from './contentful-navigation-action-content'
